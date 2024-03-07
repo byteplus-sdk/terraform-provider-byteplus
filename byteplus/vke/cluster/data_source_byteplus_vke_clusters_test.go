c2e07b49fed126c1fed31083313b5d16c17eb33b
maoshuai.17 <maoshuai.17@bytedance.com>
2024-03-07 17:51:29 +0800

    feat: support vke cluster and node pool

---
+++ byteplus/vke/cluster/data_source_byteplus_vke_clusters_test.go
@@ -0,0 +1,88 @@
+package cluster_test
+
+import (
+	"github.com/byteplus-sdk/terraform-provider-byteplus/byteplus"
+	"github.com/byteplus-sdk/terraform-provider-byteplus/byteplus/vke/cluster"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"testing"
+)
+
+const testAccByteplusVkeClustersDatasourceConfig = `
+data "byteplus_zones" "foo"{
+}
+
+resource "byteplus_vpc" "foo" {
+    vpc_name = "acc-test-project1"
+    cidr_block = "172.16.0.0/16"
+}
+
+resource "byteplus_subnet" "foo" {
+    subnet_name = "acc-subnet-test-2"
+    cidr_block = "172.16.0.0/24"
+    zone_id = data.byteplus_zones.foo.zones[0].id
+    vpc_id = byteplus_vpc.foo.id
+}
+
+resource "byteplus_security_group" "foo" {
+    vpc_id = byteplus_vpc.foo.id
+    security_group_name = "acc-test-security-group2"
+}
+
+resource "byteplus_vke_cluster" "foo" {
+    name = "acc-test-1"
+    description = "created by terraform"
+    delete_protection_enabled = false
+    cluster_config {
+        subnet_ids = [byteplus_subnet.foo.id]
+        api_server_public_access_enabled = true
+        api_server_public_access_config {
+            public_access_network_config {
+                billing_type = "PostPaidByBandwidth"
+                bandwidth = 1
+            }
+        }
+        resource_public_access_default_enabled = false
+    }
+    pods_config {
+        pod_network_mode = "VpcCniShared"
+        vpc_cni_config {
+            subnet_ids = [byteplus_subnet.foo.id]
+        }
+    }
+    services_config {
+        service_cidrsv4 = ["172.30.0.0/18"]
+    }
+    tags {
+        key = "tf-k1"
+        value = "tf-v1"
+    }
+}
+
+data "byteplus_vke_clusters" "foo"{
+    ids = [byteplus_vke_cluster.foo.id]
+}
+`
+
+func TestAccByteplusVkeClustersDatasource_Basic(t *testing.T) {
+	resourceName := "data.byteplus_vke_clusters.foo"
+
+	acc := &byteplus.AccTestResource{
+		ResourceId: resourceName,
+		Svc:        &cluster.ByteplusVkeClusterService{},
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			byteplus.AccTestPreCheck(t)
+		},
+		Providers: byteplus.GetTestAccProviders(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccByteplusVkeClustersDatasourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(acc.ResourceId, "clusters.#", "1"),
+				),
+			},
+		},
+	})
+}
---
+++ byteplus/vke/node/resource_byteplus_vke_node.go
@@ -0,0 +1,212 @@
+package node
+
+import (
+	"fmt"
+	"time"
+
+	bp "github.com/byteplus-sdk/terraform-provider-byteplus/common"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+/*
+
+Import
+VKE node can be imported using the node id, e.g.
+```
+$ terraform import byteplus_vke_node.default nc5t5epmrsf****
+```
+
+*/
+
+func ResourceByteplusVkeNode() *schema.Resource {
+	resource := &schema.Resource{
+		Create: resourceByteplusVkeNodeCreate,
+		Read:   resourceByteplusVkeNodeRead,
+		Update: resourceByteplusVkeNodeUpdate,
+		Delete: resourceByteplusVkeNodeDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(30 * time.Minute),
+		},
+		Schema: map[string]*schema.Schema{
+			"client_token": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Computed:    true,
+				Description: "The client token.",
+			},
+			"cluster_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The cluster id.",
+			},
+			"instance_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The instance id.",
+			},
+			"keep_instance_name": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					return len(d.Id()) != 0
+				},
+				Description: "The flag of keep instance name, the value is `true` or `false`.",
+			},
+			"additional_container_storage_enabled": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				ForceNew:    true,
+				Description: "The flag of additional container storage enable, the value is `true` or `false`.",
+			},
+			"image_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "The ImageId of NodeConfig.",
+			},
+			"initialize_script": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "The initializeScript of Node.",
+			},
+			"kubernetes_config": {
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Optional:    true,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "The KubernetesConfig of Node.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"labels": {
+							Type:     schema.TypeList,
+							Optional: true,
+							ForceNew: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"key": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										ForceNew:    true,
+										Description: "The Key of Labels.",
+									},
+									"value": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										ForceNew:    true,
+										Description: "The Value of Labels.",
+									},
+								},
+							},
+							Description: "The Labels of KubernetesConfig.",
+						},
+						"taints": {
+							Type:     schema.TypeList,
+							Optional: true,
+							ForceNew: true,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"key": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										ForceNew:    true,
+										Description: "The Key of Taints.",
+									},
+									"value": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										ForceNew:    true,
+										Description: "The Value of Taints.",
+									},
+									"effect": {
+										Type:        schema.TypeString,
+										Optional:    true,
+										ForceNew:    true,
+										Description: "The Effect of Taints, the value can be `NoSchedule` or `NoExecute` or `PreferNoSchedule`.",
+									},
+								},
+							},
+							Description: "The Taints of KubernetesConfig.",
+						},
+						"cordon": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							ForceNew:    true,
+							Computed:    true,
+							Description: "The Cordon of KubernetesConfig.",
+						},
+					},
+				},
+			},
+			"container_storage_path": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					flag := d.Get("additional_container_storage_enabled")
+					return flag == nil || !flag.(bool)
+				},
+				Description: "The container storage path.",
+			},
+			"node_pool_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Computed:    true,
+				Description: "The node pool id.",
+			},
+		},
+	}
+	return resource
+}
+
+func resourceByteplusVkeNodeCreate(d *schema.ResourceData, meta interface{}) (err error) {
+	nodeService := NewByteplusVkeNodeService(meta.(*bp.SdkClient))
+	err = bp.DefaultDispatcher().Create(nodeService, d, ResourceByteplusVkeNode())
+	if err != nil {
+		return fmt.Errorf("error on creating vke node  %q, %s", d.Id(), err)
+	}
+	return resourceByteplusVkeNodeRead(d, meta)
+}
+
+func resourceByteplusVkeNodeRead(d *schema.ResourceData, meta interface{}) (err error) {
+	nodeService := NewByteplusVkeNodeService(meta.(*bp.SdkClient))
+	err = bp.DefaultDispatcher().Read(nodeService, d, ResourceByteplusVkeNode())
+	if err != nil {
+		return fmt.Errorf("error on reading vke node %q, %s", d.Id(), err)
+	}
+	return err
+}
+
+func resourceByteplusVkeNodeUpdate(d *schema.ResourceData, meta interface{}) (err error) {
+	nodeService := NewByteplusVkeNodeService(meta.(*bp.SdkClient))
+	err = bp.DefaultDispatcher().Update(nodeService, d, ResourceByteplusVkeNode())
+	if err != nil {
+		return fmt.Errorf("error on updating vke node  %q, %s", d.Id(), err)
+	}
+	return resourceByteplusVkeNodeRead(d, meta)
+}
+
+func resourceByteplusVkeNodeDelete(d *schema.ResourceData, meta interface{}) (err error) {
+	nodeService := NewByteplusVkeNodeService(meta.(*bp.SdkClient))
+	err = bp.DefaultDispatcher().Delete(nodeService, d, ResourceByteplusVkeNode())
+	if err != nil {
+		return fmt.Errorf("error on deleting vke node %q, %s", d.Id(), err)
+	}
+	return err
+}
